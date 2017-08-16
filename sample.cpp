6b5284cb53e3d6a92d8d4be1e764003d72192dc1
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2017-08-16 18:50:36 +0900

    check vector endian

--- sample.cpp
+++ sample.cpp
@@ -1,12 +1,22 @@
 #include <msa.h>
 #include "dumpVector.hpp"
 
+const unsigned int endianCheck[] = {0x1234567, 0x89abcdef01, 0xacacacac, 0x559f559f};
 const short src1[] = {1, 2, 3, 4, 5, 6, 7, 8, };
 const short src2[] = {100, 100, 100, 100, 100, 100, 100, 100,};
 short dst[] = {0, 0, 0, 0, 0, 0, 0, 0, };
 
 int main(int argc, char**argv)
 {
+	std::cout << std::hex << "0x" << endianCheck[0] << std::endl;
+	std::cout << std::hex << "0x" << endianCheck[1] << std::endl;
+	std::cout << std::hex << "0x" << endianCheck[2] << std::endl;
+	std::cout << std::hex << "0x" << endianCheck[3] << std::endl;
+	v4u32 v_endian = __msa_ld_w((void*)endianCheck, 0);
+	dumpVector(v_endian);
+	v16u8 v_endianChar = (v16u8)v_endian;
+	dumpVector(v_endianChar);
+
 	unsigned char* foo = (unsigned char*)src1;
 	for(auto i = 0;i < 16;i++)
 	{
