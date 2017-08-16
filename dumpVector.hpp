aadb33bd747739fdeae3076fdf6e189b7592fcbb
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2017-08-16 18:05:13 +0900

    remember that it's a big endian

--- dumpVector.hpp
+++ dumpVector.hpp
@@ -2,102 +2,28 @@
 #include <iostream>
 #include <iomanip>
 
-template <typename T, typename Tp>
-void dumpVector(T a)
-{
-	unsigned char stub[16];
-	__msa_st_b((v16i8)a, (void*)stub, 0);
-
-	Tp* dump = (Tp*)stub;
-	const int cLength = 16 / sizeof(Tp);
-	std::cout << "========================================" << std::endl;
-	for(auto i = 0;i < cLength;i++)
-	{
-		std::cout << i << ": " << dump[i] << std::endl;
-	}
+#define DUMP_VECTOR(vec0,vec1,type,suffix) \
+void dumpVector(vec0 a)                    \
+{                                          \
+	unsigned char stub[16];                \
+	__msa_st_##suffix((vec1)a, (void*)stub, 0);\
+	Tp* dump = (Tp*)stub;                  \
+	const int cLength = 16 / sizeof(type); \
+	std::cout << "========================================" << std::endl; \
+	for(auto i = 0;i < cLength;i++)        \
+	{                                      \
+		std::cout << i << ": " << dump[i] << std::endl; \
+	}                                      \
 }
 
-struct _f64u64
-{
-	union
-	{
-		double d;
-		unsigned long long u;
-	};
-};
-typedef struct _f64u64 f64u64;
-
-struct _f32u32
-{
-	union
-	{
-		float f;
-		unsigned u;
-	};
-};
-typedef struct _f32u32 f32u32;
-
-template <>
-void dumpVector<v2f64, double>(v2f64 a)
-{
-	unsigned char stub[16];
-	__msa_st_b((v16i8)a, (void*)stub, 0);
-
-	double* dump = (double*)stub;
-	const int cLength = 16 / sizeof(double);
-	std::cout << "****************************************" << std::endl;
-	for(auto i = 0;i < cLength;i++)
-	{
-		f64u64 x;
-		x.d = dump[i];
-		std::cout << i << ": 0x" << std::hex << x.u << std::endl;
-	}
-}
-
-template <>
-void dumpVector<v4f32, float>(v4f32 a)
-{
-	unsigned char stub[16];
-	__msa_st_b((v16i8)a, (void*)stub, 0);
-
-	float* dump = (float*)stub;
-	const int cLength = 16 / sizeof(float);
-	std::cout << "****************************************" << std::endl;
-	for(auto i = 0;i < cLength;i++)
-	{
-		f32u32 x;
-		x.f = dump[i];
-		std::cout << i << ": 0x" << std::hex << x.u << std::endl;
-	}
-}
-
-template <>
-void dumpVector<v16u8, unsigned char>(v16u8 a)
-{
-	unsigned char stub[16];
-	__msa_st_b((v16i8)a, (void*)stub, 0);
-
-	char* dump = (char*)stub;
-	const int cLength = 16 / sizeof(char);
-	std::cout << "****************************************" << std::endl;
-	for(auto i = 0;i < cLength;i++)
-	{
-		std::cout << i << ": 0x" << std::hex << (int)dump[i] << std::endl;
-	}
-}
-
-template <>
-void dumpVector<v16i8, char>(v16i8 a)
-{
-	unsigned char stub[16];
-	__msa_st_b(a, (void*)stub, 0);
-
-	char* dump = (char*)stub;
-	const int cLength = 16 / sizeof(char);
-	std::cout << "****************************************" << std::endl;
-	for(auto i = 0;i < cLength;i++)
-	{
-		std::cout << i << ": 0x" << std::hex << (int)dump[i] << std::endl;
-	}
-}
+DUMP_VECTOR(v16i8,v16i8,char,b);
+DUMP_VECTOR(v16u8,v16i8,unsigned char,b);
+DUMP_VECTOR(v8i16,v8i16,short,h);
+DUMP_VECTOR(v8u16,v8i16,unsigned short,h);
+DUMP_VECTOR(v4i32,v4i32,int,w);
+DUMP_VECTOR(v4u32,v4i32,unsigned,w);
+DUMP_VECTOR(v4f32,v4i32,float,w);
+DUMP_VECTOR(v2i64,v2i64,long long,d);
+DUMP_VECTOR(v2u64,v2i64,unsigned long long,d);
+DUMP_VECTOR(v2f64,v2i64,double,d);
 
