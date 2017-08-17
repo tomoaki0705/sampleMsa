f03d46f28912e377f7a853a48077be05675ab21e
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2017-08-17 12:41:17 +0900

    switch from mips to mipsel   * switch from big endian to little endian

--- environmentVariables.sh
+++ environmentVariables.sh
@@ -1,3 +1,3 @@
-export QEMU_LD_PREFIX=/opt/cross/mips-linux
+export QEMU_LD_PREFIX=/opt/cross/mipsel-linux
 export QEMU_CPU=P5600
 
--- mips.toolchain.cmake
+++ mips.toolchain.cmake
@@ -2,12 +2,12 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /opt/cross/bin/mips-linux-gcc-7.1.0)
-SET(CMAKE_CXX_COMPILER /opt/cross/bin/mips-linux-g++)
+SET(CMAKE_C_COMPILER   /opt/cross/bin/mipsel-linux-gcc-7.1.0)
+SET(CMAKE_CXX_COMPILER /opt/cross/bin/mipsel-linux-g++)
 
 # where is the target environment 
 #SET(CMAKE_FIND_ROOT_PATH  /opt/cross /opt/cross/mips-linux)
-SET(CMAKE_FIND_ROOT_PATH  /opt/cross/mips-linux)
+SET(CMAKE_FIND_ROOT_PATH  /opt/cross/mipsel-linux)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
