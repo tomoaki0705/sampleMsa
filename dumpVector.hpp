b7fecdeac8d7df6d2fb6d70a75bae8da7e0d0efe
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2017-08-17 18:06:54 +0900

    seems something wrong with the output of compiler

--- dumpVector.hpp
+++ dumpVector.hpp
@@ -2,6 +2,29 @@
 #include <iostream>
 #include <iomanip>
 
+struct _f32u32
+{
+	union
+	{
+		float f;
+		int i;
+		unsigned u;
+	};
+};
+
+struct _f64u64
+{
+	union
+	{
+		double d;
+		long long i;
+		unsigned long long u;
+	};
+};
+
+typedef struct _f32u32 f32u32;
+typedef struct _f64u64 f64u64;
+
 #define DEFAULT_DUMP_16(vec) \
 	std::cout << "15: 0x" << std::hex << std::setw(2) << std::setfill('0') << __msa_copy_u_b((v16i8)vec,15) << ' '; \
 	std::cout << "14: 0x" << std::hex << std::setw(2) << std::setfill('0') << __msa_copy_u_b((v16i8)vec,14) << ' '; \
@@ -30,6 +53,14 @@
 	std::cout << "1: 0x" << std::hex << std::setw(4) << std::setfill('0') << __msa_copy_u_h((v8i16)vec,1) << ' '; \
 	std::cout << "0: 0x" << std::hex << std::setw(4) << std::setfill('0') << __msa_copy_u_h((v8i16)vec,0) << std::endl;
 
+#define DEFAULT_DUMP_4_FLOAT(vec) \
+	f32u32 s; \
+	std::cout << std::dec; \
+	s.i = __msa_copy_u_w((v4i32)vec,3);std::cout << "3: " << (float)s.f << ' ' << s.u << ' '; \
+	s.i = __msa_copy_u_w((v4i32)vec,2);std::cout << "2: " << (float)s.f << ' ' << s.u << ' '; \
+	s.i = __msa_copy_u_w((v4i32)vec,1);std::cout << "1: " << (float)s.f << ' ' << s.u << ' '; \
+	s.i = __msa_copy_u_w((v4i32)vec,0);std::cout << "0: " << (float)s.f << ' ' << s.u << std::endl;
+
 #define DEFAULT_DUMP_4(vec) \
 	std::cout << "3: 0x" << std::hex << std::setw(8) << std::setfill('0') << __msa_copy_u_w((v4i32)vec,3) << ' '; \
 	std::cout << "2: 0x" << std::hex << std::setw(8) << std::setfill('0') << __msa_copy_u_w((v4i32)vec,2) << ' '; \
@@ -46,70 +77,60 @@ void dumpVector(T a);
 template <>
 void dumpVector<v16i8>(v16i8 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_16(a);
 }
 
 template <>
 void dumpVector<v16u8>(v16u8 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_16(a);
 }
 
 template <>
 void dumpVector<v8i16>(v8i16 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_8(a);
 }
 
 template <>
 void dumpVector<v8u16>(v8u16 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_8(a);
 }
 
 template <>
 void dumpVector<v4i32>(v4i32 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_4(a);
 }
 
 template <>
 void dumpVector<v4u32>(v4u32 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_4(a);
 }
 
 template <>
 void dumpVector<v4f32>(v4f32 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_4(a);
 }
 
 template <>
 void dumpVector<v2i64>(v2i64 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_2(a);
 }
 
 template <>
 void dumpVector<v2u64>(v2u64 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_2(a);
 }
 
 template<>
 void dumpVector<v2f64>(v2f64 a)
 {
-	std::cout << "====================" << std::endl;
 	DEFAULT_DUMP_2(a);
 }
 
@@ -117,7 +138,6 @@ template<typename T>
 void dumpArray(T* a)
 {
 	auto cLength = 16/sizeof(T);
-	std::cout << "====================" << std::endl;
 	for(auto i = 0;i < cLength;i++)
 	{
 		std::cout << i << ": 0x" << std::hex << a[i] << ' ';
@@ -129,7 +149,6 @@ template<>
 void dumpArray<unsigned char>(unsigned char* a)
 {
 	auto cLength = 16/sizeof(unsigned char);
-	std::cout << "====================" << std::endl;
 	for(auto i = 0;i < cLength;i++)
 	{
 		std::cout << i << ": 0x" << std::hex << std::setw(2) << std::setfill('0') << (int)a[i] << ' ';
--- sample.cpp
+++ sample.cpp
@@ -25,6 +25,7 @@ void v_pack()
 	dumpVector(v_pack02);
 	dumpVector(v_pack03);
 
+	std::cout << "================" << std::endl;
 	v4i32 v_pack11 = __msa_ld_w((void*)pack11, 0);
 	v4i32 v_pack12 = __msa_ld_w((void*)pack12, 0);
 	v8i16 v_pack13 = __msa_pckev_h((v8i16)v_pack12, (v8i16)v_pack11);
@@ -32,6 +33,7 @@ void v_pack()
 	dumpVector(v_pack12);
 	dumpVector(v_pack13);
 
+	std::cout << "================" << std::endl;
 	v8i16 v_pack21 = __msa_ld_h((void*)pack21, 0);
 	v8i16 v_pack22 = __msa_ld_h((void*)pack22, 0);
 	v16i8 v_pack23 = __msa_pckev_b((v16i8)v_pack22, (v16i8)v_pack21);
@@ -50,6 +52,7 @@ void v_pack_store()
 	__msa_st_w(v_pack03, (void*)v_dst04, 0);
 	dumpArray(v_dst04);
 
+	std::cout << "================" << std::endl;
 	v4i32 v_pack11 = __msa_ld_w((void*)pack11, 0);
 	v4i32 v_pack12 = __msa_ld_w((void*)pack12, 0);
 	v8i16 v_pack13 = __msa_pckev_h((v8i16)v_pack12, (v8i16)v_pack11);
@@ -57,6 +60,7 @@ void v_pack_store()
 	__msa_st_h(v_pack13, (void*)v_dst14, 0);
 	dumpArray(v_dst14);
 
+	std::cout << "================" << std::endl;
 	v8i16 v_pack21 = __msa_ld_h((void*)pack21, 0);
 	v8i16 v_pack22 = __msa_ld_h((void*)pack22, 0);
 	v16i8 v_pack23 = __msa_pckev_b((v16i8)v_pack22, (v16i8)v_pack21);
@@ -65,16 +69,131 @@ void v_pack_store()
 	dumpArray(v_dst24);
 }
 
+// shift right and round and then pack
+void v_rshr_pack()
+{
+	const long long pack01s[] = {-33, -34, };
+	const long long pack02s[] = {-35, -36, };
+	const int pack11s[] = {-1, -2, -3, -4};
+	const int pack12s[] = {-37, -38, -39, -40};
+	const short pack21s[] = {-1, -2, -3, -4, -5, -6, -7, -8};
+	const short pack22s[] = {-41, -42, -43, -44, -45, -46, -47, -48};
+	const int iShift = 4;
+
+	v2i64 v_delta0 = __msa_fill_d(1);
+	v2i64 v_pack01 = __msa_ld_d((void*)pack01s, 0);
+	v2i64 v_pack02 = __msa_ld_d((void*)pack02s, 0);
+	v4i32 v_pack03 = __msa_pckev_w((v4i32)((v_pack02 + (v_delta0 << (iShift - 1))) >> iShift), (v4i32)((v_pack01 + (v_delta0 << (iShift - 1))) >> iShift));
+	dumpVector(v_pack01);
+	dumpVector(v_pack02);
+	dumpVector(v_pack03);
+
+	std::cout << "================" << std::endl;
+	v4i32 v_delta1 = __msa_fill_w(1);
+	v4i32 v_pack11 = __msa_ld_w((void*)pack11s, 0);
+	v4i32 v_pack12 = __msa_ld_w((void*)pack12s, 0);
+	v8i16 v_pack13 = __msa_pckev_h((v8i16)((v_pack12 + (v_delta1 << (iShift - 1))) >> iShift), (v8i16)((v_pack11 + (v_delta1 << (iShift - 1))) >> iShift));
+	dumpVector(v_pack11);
+	dumpVector(v_pack12);
+	dumpVector(v_pack13);
+
+	std::cout << "================" << std::endl;
+	v8i16 v_delta2 = __msa_fill_h(1);
+	v8i16 v_pack21 = __msa_ld_h((void*)pack21s, 0);
+	v8i16 v_pack22 = __msa_ld_h((void*)pack22s, 0);
+	v16i8 v_pack23 = __msa_pckev_b((v16i8)((v_pack22 + (v_delta2 << (iShift - 1))) >> iShift), (v16i8)((v_pack21 + (v_delta2 << (iShift - 1))) >> iShift));
+	dumpVector(v_pack11);
+	dumpVector(v_pack12);
+	dumpVector(v_pack13);
+}
+
+// shift right and round and then pack
+void v_rshr_packu()
+{
+	const int iShift = 4;
+
+	v2i64 v_delta0 = __msa_fill_d(1);
+	v2i64 v_pack01 = __msa_ld_d((void*)pack01, 0);
+	v2i64 v_pack02 = __msa_ld_d((void*)pack02, 0);
+	v4i32 v_pack03 = __msa_pckev_w((v4i32)((v_pack02 + (v_delta0 << (iShift - 1))) >> iShift), (v4i32)((v_pack01 + (v_delta0 << (iShift - 1))) >> iShift));
+	dumpVector(v_pack01);
+	dumpVector(v_pack02);
+	dumpVector(v_pack03);
+
+	std::cout << "================" << std::endl;
+	v4i32 v_delta1 = __msa_fill_w(1);
+	v4i32 v_pack11 = __msa_ld_w((void*)pack11, 0);
+	v4i32 v_pack12 = __msa_ld_w((void*)pack12, 0);
+	v8i16 v_pack13 = __msa_pckev_h((v8i16)((v_pack12 + (v_delta1 << (iShift - 1))) >> iShift), (v8i16)((v_pack11 + (v_delta1 << (iShift - 1))) >> iShift));
+	dumpVector(v_pack11);
+	dumpVector(v_pack12);
+	dumpVector(v_pack13);
+
+	std::cout << "================" << std::endl;
+	v8i16 v_delta2 = __msa_fill_h(1);
+	v8i16 v_pack21 = __msa_ld_h((void*)pack21, 0);
+	v8i16 v_pack22 = __msa_ld_h((void*)pack22, 0);
+	v16i8 v_pack23 = __msa_pckev_b((v16i8)((v_pack22 + (v_delta2 << (iShift - 1))) >> iShift), (v16i8)((v_pack21 + (v_delta2 << (iShift - 1))) >> iShift));
+	dumpVector(v_pack11);
+	dumpVector(v_pack12);
+	dumpVector(v_pack13);
+}
+
+void v_matmul()
+{
+	// only for v4f32
+	const float m0[] = {1.f,  2.f,  3.f,  4.f, };
+	const float m1[] = {5.f,  6.f,  7.f,  8.f, };
+	const float m2[] = {11.f, 12.f, 13.f, 14.f, };
+	const float m3[] = {10.f, 20.f, 30.f, 40.f, };
+	const float v[]  = {1.f,  2.f,  3.f,  4.f, };
+
+	v4f32 v_m0 = (v4f32)__msa_ld_w((void*)m0, 0);
+	v4f32 v_m1 = (v4f32)__msa_ld_w((void*)m1, 0);
+	v4f32 v_m2 = (v4f32)__msa_ld_w((void*)m2, 0);
+	v4f32 v_m3 = (v4f32)__msa_ld_w((void*)m3, 0);
+	v4f32 v_v  = (v4f32)__msa_ld_w((void*)v, 0);
+
+	dumpVector(v_v);
+	dumpVector(__msa_splati_w((v4i32)v_v, 0));
+	dumpVector(v_m0);
+	v_m0 = v_m0 * (v4f32)__msa_splati_w((v4i32)v_v, 0);
+	v_m1 = v_m1 * (v4f32)__msa_splati_w((v4i32)v_v, 1);
+	v_m2 = v_m2 * (v4f32)__msa_splati_w((v4i32)v_v, 2);
+	v_m3 = v_m3 * (v4f32)__msa_splati_w((v4i32)v_v, 3);
+	dumpVector(v_m0);
+	dumpVector(v_m1);
+	dumpVector(v_m2);
+	dumpVector(v_m3);
+
+	v4f32 result = v_m0 + v_m1 + v_m2 + v_m3;
+	dumpVector(result);
+	
+}
+
+// shift right and round and pack and then store
+
 int main(int argc, char**argv)
 {
+	std::cout << "======== v_pack ========" << std::endl;
 	v_pack();
 
+	std::cout << "======== v_pack_store ========" << std::endl;
 	v_pack_store();
 
-	v4u32 v_endian = (v4u32)__msa_ld_w((void*)endianCheck, 0);
-	dumpVector(v_endian);
-	v16u8 v_endianChar = (v16u8)v_endian;
-	dumpVector(v_endianChar);
+	std::cout << "======== v_rshr_pack ========" << std::endl;
+	v_rshr_pack();
+
+	std::cout << "======== v_rshr_packu ========" << std::endl;
+	v_rshr_packu();
+
+	std::cout << "======== v_matmul ========" << std::endl;
+	v_matmul();
+
+	//v4u32 v_endian = (v4u32)__msa_ld_w((void*)endianCheck, 0);
+	//dumpVector(v_endian);
+	//v16u8 v_endianChar = (v16u8)v_endian;
+	//dumpVector(v_endianChar);
 
 	//inline _Tpvec v_##pack(const _Tpwvec& a, const _Tpwvec& b) \
 	//{ \
@@ -101,22 +220,22 @@ int main(int argc, char**argv)
 	//}
 
 
-	v_endianChar = v_endianChar << 3;
-	dumpVector(v_endianChar);
+	//v_endianChar = v_endianChar << 3;
+	//dumpVector(v_endianChar);
 
-	unsigned char* foo = (unsigned char*)src1;
-	for(auto i = 0;i < 16;i++)
-	{
-		std::cout << i << "\t: " << (int)foo[i] << std::endl;
-	}
-	v8i16 v_src1 = (v8i16)__msa_ld_h((void*)src1, 0);
-	v8i16 v_src2 = (v8i16)__msa_ld_h((void*)src2, 0);
+	//unsigned char* foo = (unsigned char*)src1;
+	//for(auto i = 0;i < 16;i++)
+	//{
+	//	std::cout << i << "\t: " << (int)foo[i] << std::endl;
+	//}
+	//v8i16 v_src1 = (v8i16)__msa_ld_h((void*)src1, 0);
+	//v8i16 v_src2 = (v8i16)__msa_ld_h((void*)src2, 0);
 
-	dumpVector(v_src1);
+	//dumpVector(v_src1);
 
-	v8i16 v_dst = v_src1 + v_src2;
+	//v8i16 v_dst = v_src1 + v_src2;
 
-	dumpVector(v_dst);
+	//dumpVector(v_dst);
 
 	return 0;
 }
